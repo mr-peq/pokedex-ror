8f281df7fef2f7c49c9ce2c12e8c50c6f4908d66
Theodore Saulnier <theodore1712@yahoo.fr>
2024-01-24 12:16:00 +0100

    [Controllers] Added method to handle exclusion queries

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,6 +4,8 @@ class PokemonsController < ApplicationController
       @pokemons = Pokemon.all
       if params[:types].present?
         @pokemons = filtered_pokemons
+      elsif params[:not_types].present?
+        @pokemons = exclude_query
       end
     rescue => exception
       @errors = exception
@@ -24,6 +26,23 @@ class PokemonsController < ApplicationController
     types.each do |type|
       pokemons.push(Pokemon.search(type))
     end
+    pokemons.flatten.uniq.sort_by(&:index)
+  end
+
+  def exclude_query
+    all_types = Type.all.map(&:name)
+    excluded_types = params[:not_types].split
+    types = all_types - excluded_types
+    pokemons = []
+    types.each do |type|
+      matching_pokemons = Pokemon.search(type)
+      matching_pokemons.each do |pokemon|
+        pokemon_types = pokemon.types.map(&:name)
+        has_excluded_type = pokemon_types & excluded_types
+        pokemons << pokemon unless has_excluded_type.length.positive?
+      end
+    end
+
     pokemons.flatten.uniq.sort_by(&:index)
   end
 end
